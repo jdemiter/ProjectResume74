f927063d14387c4037c36bc477179722d7e23de2
Brian <Brian@JOnessolutions.com>
2017-04-01 11:36:34 -0600

    second committ

---
+++ Models/JobDuty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectResume.Models
+{
+    public class JobDuty
+    {
+        public int ID { get; set; }
+
+        //JobDuty properties
+
+        public int WorkExperienceID { get; set; }
+        [ForeignKey("WorkExperienceId")]
+        public WorkExperience Job { get; set; }
+    }
+}
